e45ccd4e86bf26727c592d89b39622ed5a0c5405
Joshua Klingelhafer <klingejh@mail.uc.edu>
2024-12-06 12:33:16 -0500

    Commit to  add txtPackage function which is used to show the location for the photo.

--- CompleteDuck_FinalProject/mainPackage/main.py
+++ CompleteDuck_FinalProject/mainPackage/main.py
@@ -1,5 +1,31 @@
 #main.py
 
 from photoPackage.photo import *
+from txtPackage.txt import *
+if __name__ == "__main__":
+    
+    printPhoto("BuildingAndMovieQuote.jpg")
+    
+    data = read_txt_to_list('./Data/UCEnglish.txt')
+    indices = [7480, 28894, 8018, 46342, 42061, 103568, 346, 13068, 23887, 21407]
+    Location = [data[i] for i in indices]
 
-printPhoto("BuildingAndMovieQuote.jpg")
+    flattened_Location = []
+
+    def flatten_and_convert_to_str(item):
+        '''
+        Converts a nested list into a single flat list, and converts elements to strings after flattening.
+        @return list: the flattened list
+        '''
+        if isinstance(item, list):
+            for sub_item in item:
+                flatten_and_convert_to_str(sub_item)
+        else:
+            flattened_Location.append(str(item))
+
+    for item in Location:
+        flatten_and_convert_to_str(item)
+
+    Location_string = ' '.join(flattened_Location)
+
+    print(Location_string)
